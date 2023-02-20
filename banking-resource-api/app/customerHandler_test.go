fb50f432ab8665e476c99467e428f72d9df9d899
Sundar Rajan M <msundarrajan@gmail.com>
2023-02-20 23:03:46 +0530

    Add failing test to return empty customer array

--- banking-resource-api/app/customerHandler_test.go
+++ banking-resource-api/app/customerHandler_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Test_should_return_empty_customers_with_status_code_200(t *testing.T) {
+func Test_When_Successful_Should_Return_200_OK(t *testing.T) {
 	// customers := []dto.CustomerResponse{}
 
 	router := mux.NewRouter()
@@ -24,3 +24,29 @@ func Test_should_return_empty_customers_with_status_code_200(t *testing.T) {
 		t.Errorf("Got response code: %d", responseWriter.Code)
 	}
 }
+
+func Test_When_NoCustomers_Should_Return_Empty_Array(t *testing.T) {
+	// customers := []dto.CustomerResponse{}
+
+	router := mux.NewRouter()
+	ch := CustomerHandler{}
+	router.HandleFunc("/customers", ch.GetAllCustomers)
+
+	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
+
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got '%s'", body)
+	}
+}
+
+/*
+	Remaining tests:
+	0. Return 200 OK on success
+	1. Return empty array
+	2. Return a single customer
+	3. Return multiple customer
+	4. Return proper error message when upstream failed
+*/
