6f75d45eb019ceb4312698ec48030a9e9e2abb77
Sundar Rajan M <msundarrajan@gmail.com>
2023-02-24 00:22:18 +0530

    banking-response-api: Remove dead code

--- banking-resource-api/domain/customerRepository.go
+++ banking-resource-api/domain/customerRepository.go
@@ -5,13 +5,3 @@ import "banking-resource-api/errs"
 type CustomerRepository interface {
 	FindAll() ([]Customer, *errs.AppError)
 }
-
-type DefaultCustomerRepo struct{}
-
-func (d DefaultCustomerRepo) FindAll() ([]Customer, *errs.AppError) {
-	return []Customer{}, nil
-}
-
-func NewCustomerRepository() CustomerRepository {
-	return DefaultCustomerRepo{}
-}
