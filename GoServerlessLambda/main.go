a6998f659c53369fc57a6a1738f275119118d76d
Sundar Rajan M <msundarrajan@gmail.com>
2023-02-25 17:57:44 +0530

    Sample golang lambda

--- GoServerlessLambda/main.go
+++ GoServerlessLambda/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"net/http"
 
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/akrylysov/algnhsa"
+	"github.com/gorilla/mux"
 )
 
-func GenerateResponse(Body string, Code int) events.APIGatewayProxyResponse {
-	return events.APIGatewayProxyResponse{Body: Body, StatusCode: Code}
+//	func GenerateResponse(Body string, Code int) events.APIGatewayProxyResponse {
+//		return events.APIGatewayProxyResponse{Body: Body, StatusCode: Code}
+//	}
+//
+//	func HandleRequest(_ context.Context, request events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
+//		return GenerateResponse("Hello World", 200), nil
+//	}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(data)
 }
-func HandleRequest(_ context.Context, request events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
-	return GenerateResponse("Hello World", 200), nil
-}
+
 func main() {
-	lambda.Start(HandleRequest)
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		writeResponse(w, http.StatusOK, [1]string{"Reached Home, Congratulations"})
+	})
+
+	router.HandleFunc("/customers", func(w http.ResponseWriter, r *http.Request) {
+		writeResponse(w, http.StatusOK, [1]string{"Customers Home, Congratulations"})
+	})
+
+	algnhsa.ListenAndServe(router, nil)
+	// lambda.Start(HandleRequest)
 }
